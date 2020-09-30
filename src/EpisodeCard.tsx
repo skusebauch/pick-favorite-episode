5b5e020256c65356e2aa69dfa3bb58bd8eaa80cb
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-30 17:15:24 +0200

    added types to episode using interface IEpisode

--- src/EpisodeCard.tsx
+++ src/EpisodeCard.tsx
@@ -1,11 +1,24 @@
 import React, { useContext } from "react";
 import { Store } from "./store/Store";
 
+interface IEpisode {
+  airdate: string;
+  airstamp: string;
+  airtime: string;
+  id: number;
+  image: { medium: string; original: string };
+  name: string;
+  number: number;
+  runtime: number;
+  season: number;
+  summary: string;
+  url: string;
+}
 const EpisodeCard = () => {
   const { state } = useContext(Store);
   return (
     <section>
-      {state.episodes.map((episode: any) => {
+      {state.episodes.map((episode: IEpisode) => {
         return (
           <section key={episode.id}>
             <img
